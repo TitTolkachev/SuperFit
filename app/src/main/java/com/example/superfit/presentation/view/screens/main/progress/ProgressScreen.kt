1ba009583d521124ac869a0ebf549daddbeae321
titto <tit.tolk07022003@gmail.com>
2023-07-02 02:21:51 +0700

    Train Progress lines for exercises

--- app/src/main/java/com/example/superfit/presentation/view/screens/main/progress/ProgressScreen.kt
+++ app/src/main/java/com/example/superfit/presentation/view/screens/main/progress/ProgressScreen.kt
@@ -1,18 +1,30 @@
 package com.example.superfit.presentation.view.screens.main.progress
 
+import android.content.Context.DISPLAY_SERVICE
+import android.content.Context.WINDOW_SERVICE
+import android.graphics.Point
+import android.hardware.display.DisplayManager
+import android.os.Build
+import android.view.Display
+import android.view.WindowManager
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.superfit.R
+import com.example.superfit.presentation.theme.Pink
 import com.example.superfit.presentation.view.screens.main.progress.components.DrawExercise
 import com.example.superfit.presentation.view.screens.main.progress.components.ProgressTopBackButton
+import com.example.superfit.presentation.view.screens.main.progress.components.drawLines
 
 @Composable
 fun ProgressScreen(navController: NavController, viewModel: ProgressViewModel = hiltViewModel()) {
@@ -44,7 +56,31 @@ fun ProgressScreenContent(
             contentScale = ContentScale.FillBounds
         )
 
-        // Draw Lines and Text for Exercises
+        // One Percent of display size in pixels for lines offset
+        val windowManager = LocalContext.current.getSystemService(WINDOW_SERVICE) as WindowManager
+        val displayManager =
+            LocalContext.current.getSystemService(DISPLAY_SERVICE) as DisplayManager
+        val displaySize = getDisplaySize(windowManager, displayManager)
+        val onePercentOfDisplayHeightInPixels = with(LocalDensity.current) {
+            displaySize.first / 100f
+        }
+        val onePercentOfDisplayWidthInPixels = with(LocalDensity.current) {
+            displaySize.second / 100f
+        }
+
+        // Draw Lines for Exercises
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .drawBehind {
+                drawLines(
+                    drawScope = this,
+                    color = Pink,
+                    onePercentHeight = onePercentOfDisplayHeightInPixels,
+                    onePercentWidth = onePercentOfDisplayWidthInPixels,
+                )
+            })
+
+        // Draw Text for Exercises
         state.exercises.forEach {
             DrawExercise(it)
         }
@@ -52,4 +88,33 @@ fun ProgressScreenContent(
         // Top Button
         ProgressTopBackButton(onClick = { sendEvent(ProgressScreenIntent.NavigateBack) })
     }
+}
+
+private fun getDisplaySize(
+    windowManager: WindowManager,
+    displayManager: DisplayManager
+): Pair<Int, Int> {
+    val deviceHeight: Int
+    val deviceWidth: Int
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        val wMetrics = windowManager.currentWindowMetrics
+        val bounds = wMetrics.bounds
+        deviceHeight = bounds.height()
+        deviceWidth = bounds.width()
+    } else {
+        val displayInfo =
+            displayManager.getDisplay(
+                Display.DEFAULT_DISPLAY
+            )
+
+        val realSize = Point()
+        Display::class.java.getMethod("getRealSize", Point::class.java)
+            .invoke(displayInfo, realSize)
+        displayInfo.getRealSize(realSize)
+        deviceWidth = realSize.x
+        deviceHeight = realSize.y
+    }
+
+    return Pair(deviceHeight, deviceWidth)
 }
--- app/src/main/java/com/example/superfit/presentation/view/screens/main/progress/components/draw.kt
+++ app/src/main/java/com/example/superfit/presentation/view/screens/main/progress/components/draw.kt
@@ -1,9 +1,88 @@
 package com.example.superfit.presentation.view.screens.main.progress.components
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.dp
 import com.example.superfit.presentation.view.model.ExerciseProgress
+
+const val figmaHeight = 640f
+const val figmaWidth = 360f
+
+val circles = listOf(
+    Pair((137f) * 100 / figmaWidth, 155f * 100 / figmaHeight),
+    Pair(129f * 100 / figmaWidth, 224f * 100 / figmaHeight),
+    Pair(99f * 100 / figmaWidth, 268f * 100 / figmaHeight),
+    Pair(127f * 100 / figmaWidth, 421f * 100 / figmaHeight),
+    Pair(127f * 100 / figmaWidth, 593f * 100 / figmaHeight),
+)
+
+val lines = listOf(
+    Pair(
+        Pair(139.5f * 100 / figmaWidth, 153f * 100 / figmaHeight),
+        Pair(167f * 100 / figmaWidth, 126f * 100 / figmaHeight)
+    ),
+    Pair(
+        Pair(166.5f * 100 / figmaWidth, 126f * 100 / figmaHeight),
+        Pair(294.5f * 100 / figmaWidth, 126f * 100 / figmaHeight)
+    ),
+    Pair(
+        Pair(132.5f * 100 / figmaWidth, 224f * 100 / figmaHeight),
+        Pair(302f * 100 / figmaWidth, 224f * 100 / figmaHeight)
+    ),
+    Pair(
+        Pair(101f * 100 / figmaWidth, 271f * 100 / figmaHeight),
+        Pair(153.5f * 100 / figmaWidth, 324f * 100 / figmaHeight)
+    ),
+    Pair(
+        Pair(153f * 100 / figmaWidth, 323.5f * 100 / figmaHeight),
+        Pair(295.5f * 100 / figmaWidth, 323.5f * 100 / figmaHeight)
+    ),
+    Pair(
+        Pair(129.5f * 100 / figmaWidth, 424f * 100 / figmaHeight),
+        Pair(159f * 100 / figmaWidth, 454f * 100 / figmaHeight)
+    ),
+    Pair(
+        Pair(158.5f * 100 / figmaWidth, 453.5f * 100 / figmaHeight),
+        Pair(292f * 100 / figmaWidth, 453.5f * 100 / figmaHeight)
+    ),
+    Pair(
+        Pair(130f * 100 / figmaWidth, 592f * 100 / figmaHeight),
+        Pair(159f * 100 / figmaWidth, 563f * 100 / figmaHeight)
+    ),
+    Pair(
+        Pair(158.5f * 100 / figmaWidth, 563.5f * 100 / figmaHeight),
+        Pair(292f * 100 / figmaWidth, 563.5f * 100 / figmaHeight)
+    )
+)
 
 @Composable
 fun DrawExercise(exerciseProgress: ExerciseProgress) {
 
+
+}
+
+
+fun drawLines(drawScope: DrawScope, color: Color, onePercentHeight: Float, onePercentWidth: Float) {
+    drawScope.apply {
+        circles.forEach {
+            drawCircle(
+                color,
+                4.dp.toPx(),
+                center = Offset(it.first * onePercentWidth, it.second * onePercentHeight),
+                style = Stroke(width = 2.dp.toPx())
+            )
+        }
+
+        lines.forEach {
+            drawLine(
+                color,
+                Offset(it.first.first * onePercentWidth, it.first.second * onePercentHeight),
+                Offset(it.second.first * onePercentWidth, it.second.second * onePercentHeight),
+                strokeWidth = 2.dp.toPx()
+            )
+        }
+    }
 }
